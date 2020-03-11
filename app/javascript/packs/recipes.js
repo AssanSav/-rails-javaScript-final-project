0974cbdf8a8e93b9fbc45ea8f65effbd09900e55
AssanSav <assane.savadogo81@gmail.com>
2020-03-11 18:20:01 -0400

    seeding data for DOM test purposes

--- app/javascript/packs/recipes.js
+++ app/javascript/packs/recipes.js
@@ -3,7 +3,7 @@ let BASE_URL = "http://localhost:3000"
 class Recipe {
     constructor({ id, image_url, name, ingredients, directions, user_id }) {
         this.id = id
-        this.image = image_url
+        this.image_url = image_url
         this.name = name
         this.ingredients = ingredients
         this.directions = directions
@@ -23,6 +23,7 @@ class Recipe {
         }
     }
 }
+Recipe.all = []
 
 
 class Api {
@@ -30,10 +31,29 @@ class Api {
         return fetch(`${BASE_URL}/recipes`)
             .then(resp => resp.json())
             .then(({ data }) => {
-                return data.map(({ id, attributes: { image, name, directions, user_id } }) => {
-                    return { id, image, name, directions, user_id }
+                return data.map(({ id, attributes: { image_url, name, ingredients, directions, user_id } }) => {
+                    return { id, image_url, name, ingredients, directions, user_id }
                 })
             })
 
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    function renderRecipesIndex() {
+        Recipe.getAllRecipesFromPromise()
+        let root = document.getElementById("root")
+        Recipe.getAllRecipesFromPromise().then(recipes => {
+            recipes.forEach(({ id, image_url, name, ingredients, user_id }) => {
+                let div = document.createElement("div")
+                div.dataset["recipeid"] = id
+                div.innerHTML =     `<img src="${image_url}" alt="">
+                                    <h4>${name}</h4>
+                                    <button id="details">More Details</button>`
+                root.appendChild(div)
+            })
+        })
+    }
+    renderRecipesIndex()
+})
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.find_by(id: 1)
+
+r = u.recipes.create(image_url:"https://www.edamam.com/web-img/a8e/a8e6b4cd3856e78ad8f84f2a4d4ad8c3.jpg", name:"Beef & vegetable casserole", ingredients:" celery sticks, thickly sliced, 1 onion, chopped, 2 thyme sprigs, 1 whole and 1 leaves picked", directions:"Heat oven to 160C/140C fan/gas 3 and put the kettle on. Put 2 thickly sliced celery sticks, 1 chopped onion, 2 chunkily sliced carrots, 5 bay leaves and 1 whole thyme sprig in a flameproof casserole dish with 1 tbsp vegetable oil and 1 tbsp butter. Soften for 10 mins, then stir in 2 tbsp plain flour until it doesn’t look dusty anymore, followed by 2 tbsp tomato purée, 2 tbsp Worcestershire sauce and 2 crumbled beef stock cubes.Gradually stir in 600ml hot water, then tip in 850g stewing beef and bring to a gentle simmer.Cover and put in the oven for 2hrs 30 mins, then uncover and cook for 30mins – 1hr more until the meat is really tender and the sauce is thickened.Garnish with")
+
+c = r.comments.create(content: "I like it!!!")
+
+
+
+r1 = u.recipes.create(image_url:"https://www.edamam.com/web-img/a8e/a8e6b4cd3856e78ad8f84f2a4d4ad8c3.jpg", name:"Beef bourguignon with celeriac mash", ingredients:"tbsp goose fat
+600g shin beef, cut into large chunks
+100g smoked streaky bacon, sliced
+350g shallot
+ or pearl onions, peeled
+250g chestnut mushrooms (about 20)
+2 garlic cloves, sliced
+1 bouquet garni (See know-how below)
+1 tbsp tomato purée
+750ml bottle red wine, Burgundy is good", directions:"Heat a large casserole pan and add 1 tbsp goose fat.
+Season 600g large chunks of shin beef and fry until golden brown, about 3-5 mins, then turn over and fry the other side until the meat is browned all over, adding more fat if necessary. Do this in 2-3 batches, transferring the meat to a colander set over a bowl when browned.
+In the same pan, fry 100g sliced smoked streaky bacon, 350g peeled shallots or pearl onions, 250g chestnut mushrooms, 2 sliced garlic cloves and 1 bouquet garni until lightly browned.
+Mix in 1 tbsp tomato purée and cook for a few mins, stirring the mixture. This enriches the bourguignon and makes a great base for")
+
+
+c1 = r1.comments.create(content:"I will try that!!!!")
