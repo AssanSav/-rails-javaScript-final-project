e013db12633439967f66f122b8c8b5e633eedab6
AssanSav <assane.savadogo81@gmail.com>
2020-03-11 17:24:28 -0400

    Recipe class constructor

--- app/javascript/packs/recipes.js
+++ app/javascript/packs/recipes.js
@@ -1,1 +1,12 @@
-console.log("hello")
+let BASE_URL = "http://localhost:3000"
+
+class Recipe {
+    constructor({ id, image_url, name, ingredients, directions, user_id }) {
+        this.id = id
+        this.image = image_url
+        this.name = name
+        this.ingredients = ingredients
+        this.directions = directions
+        this.user_id = user_id
+    }
+}
