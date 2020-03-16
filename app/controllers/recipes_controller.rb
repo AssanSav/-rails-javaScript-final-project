dcb8f52080c20529e9a920ea26940d5d5cb79d76
AssanSav <assane.savadogo81@gmail.com>
2020-03-16 03:59:02 -0400

    New attributes added to strong params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -72,6 +72,6 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:name, :image_url, :ingredients, :directions, :user_id)
+      params.require(:recipe).permit(:name, :image_url, :category, :ingredient1, :unit1, :ingredient2, :unit2, :ingredient3, :unit3, :ingredient4, :unit4, :ingredient5, :unit5, :ingredient6, :unit6,  :directions, :user_id)
     end
 end
