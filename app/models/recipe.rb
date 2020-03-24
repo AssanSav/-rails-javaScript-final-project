5f4ab2e846a48a3413a4a33ff54f236fd5f1eb43
AssanSav <assane.savadogo81@gmail.com>
2020-03-24 15:52:12 -0400

    capitalizing and validating image urls before creation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,16 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  delegate :username, to: :user
   has_many :comments, dependent: :destroy
   validates :name, presence: true
+  validates :image_url, format: {with: /\.(png|jpg)\Z/i,  message: 'must be a url for gif, jpg, or png image.'}
+  before_validation :upcase_names
+
+
+  private
+
+  def upcase_names
+    self.name = self.name.capitalize
+  end
+
 end
