4410df629da8bc84d7e047b12934f323a81b5048
AssanSav <assane.savadogo81@gmail.com>
2020-03-24 16:22:01 -0400

    Refactored useless code from scaffold generator

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,56 +1,44 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
-  # GET /recipes
-  # GET /recipes.json
+ 
   def index
     @recipes = Recipe.all
     render json: RecipeSerializer.new(@recipes).serialized_json
   end
 
-  # GET /recipes/1
-  # GET /recipes/1.json
+
   def show
     option = {}
-    option[:include] = [:comments, :'comments.content', :'user_id']
+    option[:include] = [:comments, :'comments.content']
     render json: RecipeSerializer.new(@recipe, option).serialized_json
   end
 
-  # GET /recipes/new
+
   def new
     @recipe = Recipe.new
   end
 
-  # GET /recipes/1/edit
+
   def edit
   end
 
-  # POST /recipes
-  # POST /recipes.json
+
   def create
     @recipe = current_user.recipes.build(recipe_params)
-    # binding.pry
     if @recipe.save
         render json: RecipeSerializer.new(@recipe).serialized_json
     end
   end
 
-  # PATCH/PUT /recipes/1
-  # PATCH/PUT /recipes/1.json
+
   def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    if @recipe.update(recipe_params)
+        render json: RecipeSerializer.new(@recipe).serialized_json
     end
   end
 
-  # DELETE /recipes/1
-  # DELETE /recipes/1.json
+ 
   def destroy
     @recipe = current_user.recipes.find(params[:id])
     if @recipe.destroy
@@ -60,13 +48,13 @@ class RecipesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_recipe
       @recipe = Recipe.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+  
     def recipe_params
-      params.require(:recipe).permit(:name, :image_url, :category, :ingredient1, :unit1, :ingredient2, :unit2, :ingredient3, :unit3, :ingredient4, :unit4, :ingredient5, :unit5, :ingredient6, :unit6,  :directions, :user_id)
+      params.require(:recipe).permit(:id, :created_at, :updated_at, :name, :image_url, :category, :ingredient1, :unit1, :ingredient2, :unit2, :ingredient3, :unit3, :ingredient4, :unit4, :ingredient5, :unit5, :ingredient6, :unit6,  :directions, :user_id)
     end
 end
